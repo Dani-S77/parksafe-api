31458d114a6debfe0f9f64894967f05e829e32b2
Daniel Stiven Medcalfe Grisales <daniel_medcalfe@soy.sena.edu.co>
2026-04-13 19:40:40 -0500

    chore: base structure of the spring boot project

---
+++ src/test/java/com/parksafe/api/ApiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.parksafe.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
